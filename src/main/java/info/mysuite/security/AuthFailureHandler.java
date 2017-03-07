e9cc8b9261e786cf483823f43f88db4f1d430d08
Anselm Kim <anselm@aauxx.co.kr>
2017-03-07 10:53:16 +0900

    로그인 실패 시 인증 실패 코드와 메세지 리턴

--- src/main/java/info/mysuite/security/AuthFailureHandler.java
+++ src/main/java/info/mysuite/security/AuthFailureHandler.java
@@ -1,7 +1,24 @@
 package info.mysuite.security;
 
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by anselmkim on 2017. 3. 7..
  */
-public class AuthFailureHandler {
+public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.write(exception.getMessage());
+        writer.flush();
+    }
 }
