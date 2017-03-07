e699f5cd6c6a375cef7f571d490d008e0e2c99f9
Anselm Kim <anselm@aauxx.co.kr>
2017-03-07 11:09:40 +0900

    UserDetail 구현체 추가 로그아웃 성공 시 처리 로직 추가

--- src/main/java/info/mysuite/security/HttpLogoutSuccessHandler.java
+++ src/main/java/info/mysuite/security/HttpLogoutSuccessHandler.java
@@ -1,7 +1,29 @@
 package info.mysuite.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by anselmkim on 2017. 3. 7..
  */
-public class HttpLogoutSuccessHandler {
+public class HttpLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        if (authentication == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+            LoginUserDetail loginUserDetail = (LoginUserDetail) authentication.getPrincipal();
+            PrintWriter writer = response.getWriter();
+            writer.write(loginUserDetail.getUsername() + "logout success");
+            writer.flush();
+        }
+    }
 }
