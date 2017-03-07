d76c1f0771c0c9506c3573267994fcd9eb4f872d
Anselm Kim <anselm@aauxx.co.kr>
2017-03-07 10:50:39 +0900

    로그인 성공 시 status ok 리턴

--- src/main/java/info/mysuite/security/AuthSeuccessHandler.java
+++ src/main/java/info/mysuite/security/AuthSeuccessHandler.java
@@ -1,7 +1,20 @@
 package info.mysuite.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by anselmkim on 2017. 3. 7..
  */
-public class AuthSeuccessHandler {
+public class AuthSeuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
 }
