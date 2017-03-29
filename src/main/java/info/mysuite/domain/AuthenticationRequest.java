2370df83ac0fb3d2ee9aa3aedfdfd246d427a95c
Anselm Kim <anselm@aauxx.co.kr>
2017-03-29 11:59:02 +0900

    관리자 페이지 로그인 구현 중

--- src/main/java/info/mysuite/domain/AuthenticationRequest.java
+++ src/main/java/info/mysuite/domain/AuthenticationRequest.java
@@ -1,20 +1,11 @@
 package info.mysuite.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthenticationRequest {
-     
      private String username;
      private String password;
-     
-     public String getUsername() {
-          return username;
-     }
-     public void setUsername(String username) {
-          this.username = username;
-     }
-     public String getPassword() {
-          return password;
-     }
-     public void setPassword(String password) {
-          this.password = password;
-     }
 }
